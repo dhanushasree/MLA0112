9b26752db80b793a4c6b5bdaf33ddf245706e6de
dhanushasree <165866952+dhanushasree@users.noreply.github.com>
2026-06-18 14:28:13 +0530

    Delete Fruit and Color using Backtracking.pl

--- Fruit and Color using Backtracking.pl
+++
@@ -1,9 +0,0 @@
-fruit_color(apple, red).
-fruit_color(banana, yellow).
-fruit_color(grapes, green).
-fruit_color(orange, orange).
-fruit_color(mango, yellow).
-fruit_color(cherry, red).
-
-find_fruit(Fruit, Color) :-
-    fruit_color(Fruit, Color).
