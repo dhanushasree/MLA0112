1d9c48dec4b50df678a594d0819199b5998d8caa
dhanushasree <165866952+dhanushasree@users.noreply.github.com>
2026-06-18 19:55:29 +0530

    Add files via upload

---
+++ Student-Teacher-Subject.pl
@@ -0,0 +1,7 @@
+student_teacher_subject_code(dhanusha, ramesh, artificial_intelligence, ai101).
+student_teacher_subject_code(suguna, priya, machine_learning, ml102).
+student_teacher_subject_code(rahul, kumar, data_science, ds103).
+student_teacher_subject_code(anitha, lakshmi, prolog, pl104).
+
+details(Student, Teacher, Subject, Code) :-
+    student_teacher_subject_code(Student, Teacher, Subject, Code).
